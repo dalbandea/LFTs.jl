62c16b05b6049e352ed07de2f30956500f8461e9
dalbandea <albandeadavid@gmail.com>
2023-03-24 20:00:27 +0100

    organize U1hmc.jl functions into submodels

--- src/Models/U1/U1hmc.jl
+++ src/Models/U1/U1hmc.jl
@@ -1,17 +1,10 @@
+# ======================= #
+# ===== General U1 ====== #
+# ======================= #
 
 function generate_momenta!(U1ws::U1, hmcws::AbstractHMC)
     # Create momenta for U1
     hmcws.mom .= to_device(U1ws.device, randn(U1ws.PRC, size(hmcws.mom)))
-    return nothing
-end
-
-function flip_momenta_sign!(hmcws::U1quenchedHMC)
-    hmcws.mom .= .- hmcws.mom
-    return nothing
-end
-
-function flip_momenta_sign!(hmcws::U1Nf2HMC)
-    hmcws.mom .= .- hmcws.mom
     return nothing
 end
 
@@ -20,9 +13,45 @@ function Hamiltonian(U1ws::U1, hmcws::AbstractHMC)
     return H
 end
 
+function update_fields!(U1ws::T, epsilon, hmcws::AbstractHMC) where T <: U1
+    lp = U1ws.params
+    event = U1_update_field!(U1ws.device)(U1ws.U, hmcws.mom, epsilon, ndrange=(lp.iL[1], lp.iL[2]), workgroupsize=U1ws.kprm.threads)
+    wait(event)
+    return nothing
+end
+
+KernelAbstractions.@kernel function U1_update_field!(U, mom, epsilon)
+
+    i1, i2 = @index(Global, NTuple)
+
+    for id in 1:2
+        U[i1,i2,id] = complex(cos(epsilon*mom[i1,i2,id]), sin(epsilon*mom[i1,i2,id])) * U[i1,i2,id]
+    end
+end
+
+
+# ======================= #
+# ===== U1 Quenched ===== #
+# ======================= #
+
+function flip_momenta_sign!(hmcws::U1quenchedHMC)
+    hmcws.mom .= .- hmcws.mom
+    return nothing
+end
+
 function update_momenta!(U1ws::U1Quenched, epsilon, hmcws::AbstractHMC)
     force!(U1ws, hmcws)
     hmcws.mom .= hmcws.mom .+ epsilon .* (hmcws.frc1 .+ hmcws.frc2)
+    return nothing
+end
+
+
+# ======================= #
+# ====== U1 Nf = 2 ====== #
+# ======================= #
+
+function flip_momenta_sign!(hmcws::U1Nf2HMC)
+    hmcws.mom .= .- hmcws.mom
     return nothing
 end
 
@@ -55,19 +84,3 @@ function update_momenta!(U1ws::U1Nf2, epsilon, hmcws::AbstractHMC)
 end
 
 
-function update_fields!(U1ws::T, epsilon, hmcws::AbstractHMC) where T <: U1
-    lp = U1ws.params
-    event = U1_update_field!(U1ws.device)(U1ws.U, hmcws.mom, epsilon, ndrange=(lp.iL[1], lp.iL[2]), workgroupsize=U1ws.kprm.threads)
-    wait(event)
-    return nothing
-end
-
-
-KernelAbstractions.@kernel function U1_update_field!(U, mom, epsilon)
-
-    i1, i2 = @index(Global, NTuple)
-
-    for id in 1:2
-        U[i1,i2,id] = complex(cos(epsilon*mom[i1,i2,id]), sin(epsilon*mom[i1,i2,id])) * U[i1,i2,id]
-    end
-end
