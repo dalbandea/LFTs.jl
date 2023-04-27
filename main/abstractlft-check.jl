fe3129844b9d2f65adcccbb1ba4cd3cba0cbd41b
dalbandea <albandeadavid@gmail.com>
2023-04-27 15:41:41 +0200

    update main file

--- main/abstractlft-check.jl
+++ main/abstractlft-check.jl
@@ -25,14 +25,38 @@ sampler = HMC(
               ntherm = 10,
               ntraj = 100,
              )
+samplerws = LFTs.sampler(model, sampler)
+
+randomize!(model)
 
 @code_warntype sample!(model, samplerws)
+
+dHs = Vector{Float64}()
+
+for i in 1:300000
+    dH = hmc!(model, samplerws)
+    push!(dHs, dH)
+end
+
+using ADerrors
+
+phitest = "testphi14"
+uwdh = uwreal(exp.(-dHs)[10000:3:end], phitest)
+uwerr(uwdh)
+uwdh
+
+uwM = uwreal(abs.(Ms)[10000:end], phitest)
+uwerr(uwM)
+uwM
+
+taui(uwdh ,phitest)
+dtaui(uwdh ,phitest)
 
 
 # U1
 
-beta = 11.25
-lsize = 8
+beta = 5.0
+lsize = 12
 device = KernelAbstractions.CPU()
 
 model = LFTs.U1Quenched(Float64, beta = beta, iL = (lsize, lsize), device = device)
@@ -49,14 +73,34 @@ samplerws = LFTs.sampler(model, sampler)
 
 @time sample!(model, samplerws)
 
-Qs = []
+# Qs = []
 Ss = []
 
-for i in 1:1000
+
+for i in 1:1000000
+    println(i)
     @time sample!(model, samplerws)
-    push!(Qs, LFTs.top_charge(model))
-    push!(Ss, LFTs.action(model,samplerws))
+    # push!(Qs, LFTs.top_charge(model))
+    # push!(Ss, LFTs.action(model,samplerws))
+    global io = open("HMC-Qs.txt", "a")
+    println(io, LFTs.top_charge(model))
+    close(io)
 end
+
+
+uwchi = uwreal(Qs.^2/12^2, "test")
+uwerr(uwchi)
+uwchi
+taui(uwchi, "test")
+dtaui(uwchi, "test")
+
+using ADerrors
+
+uws = uwreal(Ss.^1, "test2")
+uwerr(uws)
+uws
+taui(uws, "test2")
+dtaui(uws, "test2")
 
 model_bckp = deepcopy(model)
 
